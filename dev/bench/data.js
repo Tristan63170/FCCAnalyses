5ab6be6bdccfb123d8cba36f386faff0bd65909f
github-action-benchmark <github@users.noreply.github.com>
2022-05-24 15:39:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 82a87ff68f86d22b30a9ad0e0f7a8df47527e079

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1653406769786,
+  "lastUpdate": 1653406771858,
   "repoUrl": "https://github.com/Tristan63170/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,40 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "valentin.volkl@cern.ch",
+            "name": "Valentin Volkl",
+            "username": "vvolkl"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82a87ff68f86d22b30a9ad0e0f7a8df47527e079",
+          "message": "cmake: avoid absolute paths to source headers being baked into dictionary (#178)",
+          "timestamp": "2022-05-23T16:31:54+02:00",
+          "tree_id": "7b3ff120f0118bfa6812bc28ebe1467fe21a6755",
+          "url": "https://github.com/Tristan63170/FCCAnalyses/commit/82a87ff68f86d22b30a9ad0e0f7a8df47527e079"
+        },
+        "date": 1653406771035,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 6.239920929834958,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.836137289227782,
+            "unit": "Evt/s"
           }
         ]
       }
