4394af4325d52f09a8e4234afd2052a0d43fc3bd
github-action-benchmark <github@users.noreply.github.com>
2023-08-10 07:35:30 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 5848e1e32a3cce5bb06f0cfb042c6b1b5b1070e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691652928477,
+  "lastUpdate": 1691652930554,
   "repoUrl": "https://github.com/Tristan63170/FCCAnalyses",
   "entries": {
     "Benchmark": [
@@ -1338,6 +1338,60 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/vertex_lcfiplus/analysis_V0.py"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tristan.miralles@clermont.in2p3.fr",
+            "name": "Tristan Miralles",
+            "username": "Tristan63170"
+          },
+          "committer": {
+            "email": "tristan.miralles@clermont.in2p3.fr",
+            "name": "Tristan Miralles",
+            "username": "Tristan63170"
+          },
+          "distinct": true,
+          "id": "5848e1e32a3cce5bb06f0cfb042c6b1b5b1070e3",
+          "message": "FCCAnalyses version corresponding to the ANAnote results",
+          "timestamp": "2023-08-09T15:42:39+02:00",
+          "tree_id": "299f2004bdcc0b7c4781dc2442e4f851ef3d45fc",
+          "url": "https://github.com/Tristan63170/FCCAnalyses/commit/5848e1e32a3cce5bb06f0cfb042c6b1b5b1070e3"
+        },
+        "date": 1691652929637,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Events processed per second: examples/FCCee/weaver/analysis_inference.py",
+            "value": 2.143417308949826,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: case-studies/analysis_example/scripts/analysis_example.py",
+            "value": 9.125679853580223,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu/analysis_stage1.py",
+            "value": 7.3765172316161065,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu/analysis_B2TauNu_truth.py",
+            "value": 5.868567687335862,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/test/jet_constituents.py",
+            "value": 7.3765592646078835,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "prod | Events processed per second: examples/FCCee/vertex_lcfiplus/analysis_V0.py",
+            "value": 3.005186436844125,
+            "unit": "Evt/s"
           }
         ]
       }
